15dce5e5c40b65630145d4263a776b39a21e925c
Nick Fisher <nick.fisher@avinium.com>
2024-05-10 15:04:05 +0800

    cleanup example

--- example/opusfile_dart_example.dart
+++ example/opusfile_dart_example.dart
@@ -4,12 +4,11 @@ import 'package:opusfile_dart/opusfile_dart.dart';
 
 void main() {
   var scriptDir = File(Platform.script.toFilePath()).parent.path;
-  var encoded = File("$scriptDir/test.opus").readAsBytesSync();
-  // var encoded = File(
-  //         "/Users/nickfisher/Documents/polyvox/flutter/polyvox_assets/assets/audio/1a21304defa425cd9c135bbff043c580.opus")
-  //     .readAsBytesSync();
+  // input is stereo 48kHz
+  var encoded = File("$scriptDir/test.ogg").readAsBytesSync();
   var decoder = OpusFileDecoder();
 
   var decoded = decoder.decode(encoded);
+  // output is stereo 48kHz 16-bit PCM
   File("$scriptDir/test.pcm").writeAsBytesSync(decoded.buffer.asUint8List());
 }
